1b5736ef162fbed01134308c2652397dd24831f3
Khairullah2004 <khairullahebrahimi120@gmail.com>
2024-08-13 23:44:24 +0430

    Add files via upload

---
+++ tk.py
@@ -0,0 +1,18 @@
+# import tkinter as tk
+# from tkinter import ttk
+# window=tk.Tk()
+# window.title('event binding')
+
+# entry=tk.Entry(window)
+# entry.pack()
+
+# text=tk.Text(window)
+# text.pack()
+
+# button=tk.Button(window,text='a button')
+# button.pack()
+# # button.bind('<Alt-KeyPress-a>',lambda Event: print('event'))
+# window.bind('<Motion>',lambda Event:print('event'))
+
+# window.mainloop()
+
